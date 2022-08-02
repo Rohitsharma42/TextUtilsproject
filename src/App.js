231e6ebce504ef9c1498b2d14e53740bade1bf6a
Rohitsharma42 <sharmarohit9042@gmail.com>
2022-08-02 12:00:07 +0530

    Apply switch button for enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+     setMode('dark');
+     document.body.style.backgroundColor = "#484646";
+  }
+   else{
+     setMode('light');
+     document.body.style.backgroundColor = "white";
+   }
+  }
   return (
     <>
-       <Navbar title="TextUtils" aboutText="About TextUtils"/>
+       <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
        {/* <Navbar/> */}
        <div className="container my-3">
-          <TextForm heading="Enter the text to analyze below"/>
-         
+          <TextForm heading="Enter the text to analyze below" mode={mode}/>
        </div>
-       
+       {/* <About/>  */}
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    
     const upper = () => {
         let newText = text.toUpperCase();
         setText(newText)
@@ -13,35 +14,37 @@ export default function TextForm(props) {
         let newText = ("");
         setText(newText)
     }
+    var x=document.getElementById("copy");
     const copy = () => {
        navigator.clipboard.writeText(text);
-       var x=document.getElementById("copy");
        x.style.backgroundColor="green";
        x.innerHTML=  'Copied';
     }
 
     const handleOnChange = (event) => {
-        setText(event.target.value)
+        setText(event.target.value);
+        x.style.backgroundColor="#0d6efd";
+        x.innerHTML=  'Copy to clipboard';
     }
     const [text, setText] = useState("");
     return (
         <>
-        <div className="container">
+        <div className="container" style= {{color: props.mode==='dark'?'white':'black'}}>
              <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} id="mybox" onChange={handleOnChange} rows="8"></textarea>
+                <textarea className="form-control" value={text} id="mybox" onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#484646':'white', color: props.mode==='dark'?'white':'black' }} rows="8"></textarea>
                 <button className="btn btn-primary mx-2 my-3"  onClick={upper}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2 my-3"  onClick={lower}>Convert to Lowercase</button>
                 <button className="btn btn-primary mx-2 my-3"  onClick={clear}>Clear Text</button>
                 <button id= "copy" className="btn btn-primary mx-2 my-3"  onClick={copy}>Copy to Clipboard</button>
             </div>
         </div> 
-        <div className="container">
+        <div className="container" style= {{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} words, {text.length} characters</p>
             <p>{0.008 * text.split("").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the textbox to preview it here"}</p>
         </div>
         </>
     )
